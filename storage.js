71f71aa4911d37b74bc2d75a189a009b0f7f9f9e
Rodrigo Alexandre Ribeiro Canellas <rodrigo.canellas@radixengrj.matriz>
2017-12-08 10:27:22 -0200

    Criação do arquivo 'storage.js', que implementa uma camada de abstração de acesso aos dados.

---
+++ storage.js
@@ -0,0 +1,41 @@
+var repo = require("./repository.js");
+var database = require("./database.js");
+var utils = require("./utils.js");
+
+
+//var 
+var bd = database.loadDabase("poc");
+if (bd == undefined) {
+    bd = new database.Database("poc");
+}
+
+function Storage() {
+    this.create = function(appId, body) {       
+        var doc = {};
+        doc._document = body;
+        doc._document._type= appId;
+        doc._document.id = utils.guid();
+        bd.save(doc,"master","sistema","dado salvo");
+        return doc._document.id;
+    };
+
+    this.commit = function(appId, instanceId, body) {
+        var doc = {};
+        doc._document = body;
+        doc._document._type= appId;
+        doc._document.id = instanceId;
+        bd.save(doc,"master","sistema","dado salvo");
+        return doc._document.id;        
+    };
+
+    this.findLast = function(appId) {        
+        return bd.find_all(appId);
+    };
+
+    this.findAllInstance = function(appId, instanceId) {
+        return bd.history(appId, instanceId).commits().map(c => c._data._document);
+    };
+};
+
+
+module.exports = {Storage}
