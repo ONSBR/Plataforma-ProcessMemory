79019559ba82026b6d3d3172f085e75ea914dbe2
Philippe Moneda <philippemoneda@gmail.com>
2018-01-24 12:55:43 -0200

    muda a interafce do process memory para ter apenas uma id de instancia

--- storage.js
+++ storage.js
@@ -3,37 +3,37 @@ var database = require("./database.js");
 var utils = require("./utils.js");
 
 
-//var 
+//var
 var bd = database.loadDabase("poc.db");
 if (bd == undefined) {
     bd = new database.Database("poc.db");
 }
 
 function Storage() {
-    this.create = function(appId, instanceId, body) {       
+    this.create = function(instanceId, body) {
         var doc = {};
         doc._document = body;
-        doc._document._type= appId;
+        doc._document._type= instanceId;
         doc._document.id = instanceId;
         bd.save(doc,"master","sistema","dado salvo");
         return doc._document.id;
     };
 
-    this.commit = function(appId, instanceId, body) {
+    this.commit = function(instanceId, body) {
         var doc = {};
         doc._document = body;
-        doc._document._type= appId;
+        doc._document._type= instanceId;
         doc._document.id = instanceId;
         bd.save(doc,"master","sistema","dado salvo");
-        return doc._document.id;        
+        return doc._document.id;
     };
 
-    this.head = function(appId, instanceId) {        
-        return bd.get_by_id(appId, instanceId);
+    this.head = function(instanceId) {
+        return bd.get_by_id(instanceId, instanceId);
     };
 
-    this.history = function(appId, instanceId) {
-        return bd.history(appId, instanceId).commits().map(c => {
+    this.history = function(instanceId) {
+        return bd.history(instanceId, instanceId).commits().map(c => {
             var obj = c._data._document;
             obj.timestamp = c._timestamp;
             return obj;
